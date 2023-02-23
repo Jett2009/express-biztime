f695b83ff1fbb21d86cc96bf42124391ba91b492
Jett2009 <bjett2009@gmail.com>
2023-02-23 00:54:16 -0500

    commit

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 const { Client } = require("pg");
 
 const client = new Client({
-	connectionString: "postgresql:///biztime",
+	connectionString: "postgresql://jett09:bornin91@127.0.0.1:5432/biztime",
 });
 
 client.connect();
--- routes/companies.js
+++ routes/companies.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const slugify = require("slugify");
 const ExpressError = require("../expressError");
 const db = require("../db");
 
